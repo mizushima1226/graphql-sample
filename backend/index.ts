a83f67d0791da3a466de8ec4243054dfc35f7179
みずしま <mizuki.bz.3224@gmail.com>
2021-02-06 14:31:10 +0900

    init backend project

---
+++ backend/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const router = express.Router();
+router.get("/api/user", (req: express.Request, res: express.Response) => {
+  res.send(req.query);
+});
+
+app.use(router)
+
+app.listen(3000, () => {console.log("Example app listening on port 3000!")});
