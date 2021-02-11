ecd807f5e90aad8d0e18712ca2aff4fea641e0e4
みずしま <mizuki.bz.3224@gmail.com>
2021-02-11 12:42:01 +0900

    make mock data

---
+++ backend/src/mocks/index.ts
@@ -0,0 +1,34 @@
+export const users = [
+  {
+    githubLogin: "mHattrup",
+    name: "Mike Hattrup"
+  },
+  {
+    githubLogin: "gPlake",
+    name: "Glen Plake"
+  },
+  {
+    githubLogin: "sSchmidt",
+    name: "Scot Schmidt"
+  },
+];
+
+export const photos = [
+  {
+    id: 1,
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    githubUser: "gPlake"
+  },
+  {
+    id: 2,
+    name: "Enjoying the sunshine",
+    githubUser: "sSchmidt"
+  },
+  {
+    id: 3,
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    githubUser: "sSchmidt"
+  },
+]
